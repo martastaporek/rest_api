827585cf2e781291dabd7c76232213bc5928c549
jarqprog <jarqprog@gmail.com>
2018-07-10 12:32:01 +0200

    add Match Entity

--- src/main/java/com/teamA/data/match/Match.java
+++ src/main/java/com/teamA/data/match/Match.java
@@ -3,17 +3,49 @@ package com.teamA.data.match;
 import com.teamA.data.AbstractEntity;
 import com.teamA.data.team.Team;
 
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import java.time.LocalDate;
 
+@Entity(name = "match")
 public class Match extends AbstractEntity {
 
+    @OneToOne
     private Team firstTeam;
+
+    @OneToOne
     private Team secondTeam;
+
     private int firstTeamScore;
     private int secondTeamScore;
+
+    @Temporal(TemporalType.DATE)
     private LocalDate date;
+
     private String location;
 
+    Match(Team firstTeam, Team secondTeam, String location) {
+        super();
+        this.firstTeam = firstTeam;
+        this.secondTeam = secondTeam;
+        this.location = location;
+    }
+
+    Match(Team firstTeam, Team secondTeam, int firstTeamScore,
+          int secondTeamScore, LocalDate date, String location) {
+        this.firstTeam = firstTeam;
+        this.secondTeam = secondTeam;
+        this.firstTeamScore = firstTeamScore;
+        this.secondTeamScore = secondTeamScore;
+        this.date = date;
+        this.location = location;
+    }
+
+    protected Match() {
+        super();
+    }
 
     public Team getFirstTeam() {
         return firstTeam;
