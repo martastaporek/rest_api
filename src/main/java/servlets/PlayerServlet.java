8f08b55616f7340cd14d4361b92ee827bd462e06
filipHartman <filip.hartman23@gmail.com>
2018-07-10 18:57:16 +0200

    Add servlet for players objects

--- src/main/java/servlets/PlayerServlet.java
+++ src/main/java/servlets/PlayerServlet.java
@@ -1,7 +1,22 @@
 package servlets;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class PlayerServlet extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        PrintWriter out = resp.getWriter();
+        String pathInfo = req.getPathInfo();
+
+        if(pathInfo == null || pathInfo.equals("/")) {
+
+        }
+    }
 }
