7f4ed36cf9c8a2296a0f5c42070b52b976ad3144
filipHartman <filip.hartman23@gmail.com>
2018-07-10 22:11:16 +0200

    Add test method for getting json data of player

--- src/main/java/servlets/PlayerServlet.java
+++ src/main/java/servlets/PlayerServlet.java
@@ -1,5 +1,12 @@
 package servlets;
 
+import com.google.gson.Gson;
+import com.teamA.customExceptions.PersistenceFailure;
+import com.teamA.data.player.Player;
+import com.teamA.data.player.PlayerService;
+
+
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,14 +16,28 @@ import java.io.PrintWriter;
 
 public class PlayerServlet extends HttpServlet {
 
+
+    @Inject
+    private PlayerService playerService;
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Gson gson = new Gson();
         resp.setContentType("application/json");
         PrintWriter out = resp.getWriter();
-        String pathInfo = req.getPathInfo();
-
-        if(pathInfo == null || pathInfo.equals("/")) {
-
+        String id = req.getPathInfo();
+        Player player;
+        try {
+            player = playerService.loadPlayer(String.valueOf(id));
+        } catch (PersistenceFailure persistenceFailure) {
+            persistenceFailure.printStackTrace();
+            resp.sendError(400, "Wrong URL! Usage: http://host:8080/players/{id}");
+            return;
         }
+
+        if(id == null || id.equals("/")) {
+            // tu można by zrobić wyciąganie wszystkich zawodników
+        }
+        out.println(gson.toJson(player));
     }
 }
