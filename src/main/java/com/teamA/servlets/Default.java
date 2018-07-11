dd00123c3c8ba883a96427043e1ef7d81ea38cca
staporekmarta <staporekmarta1@gmail.com>
2018-07-11 12:56:22 +0200

    Implement default servlet to connect with db

--- src/main/java/com/teamA/servlets/Default.java
+++ src/main/java/com/teamA/servlets/Default.java
@@ -25,10 +25,10 @@ public class Default extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
 
-    String dbName = "jdbc:postgresql://localhost/mundialdb";
-    String dbDriver = "org.postgresql.Driver";
-    String userName = "team_a";
-    String password = "1234";
+    private String dbName = "jdbc:postgresql://localhost/mundialDb";
+    private String dbDriver = "org.postgresql.Driver";
+    private String userName = "team_a";
+    private String password = "1234";
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
@@ -37,80 +37,12 @@ public class Default extends HttpServlet {
             Class.forName(dbDriver);
             Connection con = DriverManager.getConnection(dbName, userName, password);
             System.out.println("Got Connection");
-//            makeFunWithMatch();
-            makeFunWithPlayer();
-//            makeFunWithTeam();
+//           //method to load data
 
         } catch (SQLException | ClassNotFoundException e) {
             e.printStackTrace();
         }
     }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException{
-
-    }
-
-    private void makeFunWithPlayer() {
-
-        PlayerService playerService = Supplier.deliverPlayerService(PlayerServiceImpl.class);
-        TeamService teamService = Supplier.deliverTeamService(TeamServiceImpl.class);
-        Player loaded;
-        try {
-
-            Player newPlayer = playerService.createPlayer("Ja", "Twain", "1987");
-
-
-        } catch (PersistenceFailure persistenceFailure) {
-            persistenceFailure.printStackTrace();
-        }
-    }
-
-
-//    private void makeFunWithTeam() {
-//
-//        TeamService teamService = Supplier.deliverTeamService(TeamServiceImpl.class);
-//
-//        System.out.println(teamService.registerPlayer("1001", "100") );
-//        System.out.println(teamService.registerPlayer("1001", "101") );
-//        System.out.println(teamService.registerPlayer("1001", "102") );
-//        System.out.println(teamService.registerPlayer("1001", "103") );
-//
-//
-//        try {
-//            Team team = teamService.loadTeam("1001");
-//
-//            System.out.println(team);
-//        } catch (PersistenceFailure persistenceFailure) {
-//            persistenceFailure.printStackTrace();
-//        }
-//    }
-//
-//    private void makeFunWithMatch() {
-//
-//        MatchService matchService = Supplier.deliverMatchService(MatchServiceImpl.class);
-//        TeamService teamService = Supplier.deliverTeamService(TeamServiceImpl.class);
-//
-//        try {
-//
-//            Team team1 = teamService.createTeam("San Escobar");
-//
-//            Team team2 = teamService.createTeam("San Escobar 2");
-//
-//            Match match = matchService.createMatch(team1, team2,"Warsaw");
-//
-//
-//            System.out.println("Created match: " + match);
-//
-//            matchService.changeLocation(match, "Hamburg");
-//            matchService.registerScore(match, "0", "2");
-//
-//            System.out.println(match);
-//        } catch (PersistenceFailure persistenceFailure) {
-//            persistenceFailure.printStackTrace();
-//        }
-
-//    }
 
 
 }
