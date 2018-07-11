1b3a0bfe01ca775e3297970d85d3efbbba2154f7
filipHartman <filip.hartman23@gmail.com>
2018-07-11 23:10:41 +0200

    Add post method for match servlet

--- src/main/java/com/teamA/servlets/MatchServlet.java
+++ src/main/java/com/teamA/servlets/MatchServlet.java
@@ -1,4 +1,40 @@
 package com.teamA.servlets;
 
-public class MatchServlet {
+import com.google.gson.JsonObject;
+import com.teamA.customExceptions.PersistenceFailure;
+import com.teamA.data.match.Match;
+import com.teamA.data.match.MatchService;
+import com.teamA.parsers.JsonParser;
+import com.teamA.servletHelper.RequestDataRetriver;
+import com.teamA.servletSupplier.Supplier;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MatchServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        MatchService matchService = Supplier.deliverMatchService(MatchService.class);
+        RequestDataRetriver dataRetriever = Supplier.deliverRequestDataRetriver();
+
+        String dataFromRequest = dataRetriever.getDataFromRequest(req);
+
+        JsonParser jsonParser = Supplier.deliverJsonParser();
+        JsonObject jsonObject = jsonParser.parse(dataFromRequest);
+        Match match = jsonParser.parse(jsonObject, Match.class);
+
+        try {
+            matchService.createMatch(match.getFirstTeam(), match.getSecondTeam(), match.getLocation());
+        } catch (PersistenceFailure persistenceFailure) {
+            persistenceFailure.printStackTrace();
+        }
+    }
 }
