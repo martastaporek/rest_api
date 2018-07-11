aa023a4ea6b25bf9dcd25a4a7aee0d9127919862
filipHartman <filip.hartman23@gmail.com>
2018-07-11 10:29:57 +0200

    Make servlet method for new player and get json of existing player

--- src/main/java/com/teamA/servlets/PlayerServlet.java
+++ src/main/java/com/teamA/servlets/PlayerServlet.java
@@ -1,43 +1,69 @@
-package servlets;
+package com.teamA.servlets;
 
 import com.google.gson.Gson;
+
 import com.teamA.customExceptions.PersistenceFailure;
 import com.teamA.data.player.Player;
 import com.teamA.data.player.PlayerService;
+import com.teamA.servletSupplier.Supplier;
 
 
-import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 public class PlayerServlet extends HttpServlet {
 
 
-    @Inject
-    private PlayerService playerService;
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Gson gson = new Gson();
-        resp.setContentType("application/json");
         PrintWriter out = resp.getWriter();
-        String id = req.getPathInfo();
+        PlayerService playerService = Supplier.deliverPlayerService(PlayerService.class);
+        resp.setContentType("application/json");
+
+        String pathInfo = req.getPathInfo();
+        String id = pathInfo.replace("/", "");
+
         Player player;
         try {
-            player = playerService.loadPlayer(String.valueOf(id));
+            player = playerService.loadPlayer(id);
         } catch (PersistenceFailure persistenceFailure) {
             persistenceFailure.printStackTrace();
-            resp.sendError(400, "Wrong URL! Usage: http://host:8080/players/{id}");
+            resp.sendError(400, "Wrong URL! Usage: http://localhost:8080/players/{id}");
             return;
         }
+        Gson gson = new Gson();
 
         if(id == null || id.equals("/")) {
             // tu można by zrobić wyciąganie wszystkich zawodników
         }
         out.println(gson.toJson(player));
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        BufferedReader reader = req.getReader();
+
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while((line = reader.readLine())!= null) {
+            sb.append(line);
+        }
+
+        Gson gson = new Gson();
+        Player player = gson.fromJson(sb.toString(), Player.class);
+        System.out.println(player);
+        PlayerService playerService = Supplier.deliverPlayerService(PlayerService.class);
+        try {
+            playerService.createPlayer(player.getFirstName(), player.getLastName(), String.valueOf(player.getBirthYear()));
+        } catch (PersistenceFailure persistenceFailure) {
+            persistenceFailure.printStackTrace();
+        }
     }
 }
