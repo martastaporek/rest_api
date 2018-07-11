8b5b07617447f65acc259eb64afa45753269646a
jarqprog <jarqprog@gmail.com>
2018-07-11 10:52:02 +0200

    improve update mechanic (Entity manager use merge instead persist method while while updating)

--- src/main/java/com/teamA/serviceHelpers/ServiceTransactionHelper.java
+++ src/main/java/com/teamA/serviceHelpers/ServiceTransactionHelper.java
@@ -5,5 +5,6 @@ import com.teamA.data.AbstractEntity;
 public interface ServiceTransactionHelper {
 
     long getMaxFreeId();
-    <T extends AbstractEntity> boolean saveEntity(T entity);
+    <T extends AbstractEntity> boolean saveNewEntity(T entity);
+    <T extends AbstractEntity> boolean updateEntity(T entity);
 }
