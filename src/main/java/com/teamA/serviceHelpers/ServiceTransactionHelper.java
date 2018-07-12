7598b84447009e9ddd64629bf1568be9233fa9e9
jarqprog <jarqprog@gmail.com>
2018-07-12 13:08:17 +0200

    add removePlayer()

--- src/main/java/com/teamA/serviceHelpers/ServiceTransactionHelper.java
+++ src/main/java/com/teamA/serviceHelpers/ServiceTransactionHelper.java
@@ -1,5 +1,6 @@
 package com.teamA.serviceHelpers;
 
+import com.teamA.customExceptions.PersistenceFailure;
 import com.teamA.data.AbstractEntity;
 
 public interface ServiceTransactionHelper {
@@ -7,4 +8,5 @@ public interface ServiceTransactionHelper {
     long getMaxFreeId();
     <T extends AbstractEntity> boolean saveNewEntity(T entity);
     <T extends AbstractEntity> boolean updateEntity(T entity);
+    <T extends AbstractEntity> void removeEntity(String entityId, Class<T> entityType) throws PersistenceFailure;
 }
