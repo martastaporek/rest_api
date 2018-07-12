ffab3a7f1eb5f3c88d7eec0b3bb2076177c623bd
filipHartman <filip.hartman23@gmail.com>
2018-07-12 12:04:11 +0200

    Add getting json from database through match servlet

--- src/main/java/com/teamA/servlets/MatchServlet.java
+++ src/main/java/com/teamA/servlets/MatchServlet.java
@@ -4,6 +4,7 @@ import com.teamA.customExceptions.JsonFailure;
 import com.teamA.customExceptions.PersistenceFailure;
 import com.teamA.data.match.Match;
 import com.teamA.data.match.MatchService;
+import com.teamA.servletHelper.RequestDataRetriever;
 import com.teamA.supplier.Supplier;
 
 import javax.servlet.ServletException;
@@ -11,11 +12,34 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class MatchServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
+        resp.setContentType("application/json");
+        String id = req.getPathInfo().replace("/", "");
+        if(id == null || id.equals("/")) {
+            List<Match> matchList;
+            try {
+                matchList = Supplier.deliverMatchService(MatchService.class).loadAll();
+                resp.getWriter().println(Supplier.deliverJsonParser().parseList(matchList));
+            } catch (PersistenceFailure | JsonFailure failure) {
+                failure.printStackTrace();
+                resp.sendError(400, "Wrong URL! Usage: http://localhost:8080/matches");
+                return;
+            }
+        } else {
+            Match match;
+            try {
+                match = Supplier.deliverMatchService(MatchService.class).loadMatch(id);
+                resp.getWriter().println(Supplier.deliverJsonParser().parse(match));
+            } catch (PersistenceFailure | JsonFailure failure) {
+                failure.printStackTrace();
+                resp.sendError(400, "Wrong URL! Usage: http://localhost:8080/matches/{id}");
+                return;
+            }
+        }
     }
 
     @Override
